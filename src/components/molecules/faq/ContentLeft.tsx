967ca9804b1c458e68e182acb6ac18de3f162c04
ABDELAZIZ BOUNHAR <40790121+BounharAbdelaziz@users.noreply.github.com>
2022-01-19 23:48:19 +0100

    faq update

--- src/components/molecules/faq/ContentLeft.tsx
+++ src/components/molecules/faq/ContentLeft.tsx
@@ -9,27 +9,47 @@ const ContentRight: React.FC = () => {
       <div className="w-full pt-4">
         <div className="w-full mx-auto text-white rounded-2xl">
           <Accordion
-            title="How can I mint a Drago?"
+            title="What is Dragomania?"
             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
           />
           <hr className="border-pink"/>
           <Accordion
-            title="Can I mint on my phone?"
+            title="What are the Dragos?"
             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
           />
           <hr className="border-pink"/>
           <Accordion
-            title="What is the utility of the Dragos?"
+            title="What is Own-to-Earn?"
             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
           />
           <hr className="border-pink"/>
           <Accordion
-            title="How many Dragos can I mint at a time?"
+            title="What are the traits of Dragos?"
             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
           />
           <hr className="border-pink"/>
           <Accordion
-            title="Will there be a secondary minting phase?"
+            title="How many Dragos will there be Dragos to mint?"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
+          <hr className="border-pink"/>
+          <Accordion
+            title="WhEN MINT?"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
+          <hr className="border-pink"/>
+          <Accordion
+            title="What is the price of the Mint?"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
+          <hr className="border-pink"/>
+          <Accordion
+            title="Will there be a secondary market for Dragos?"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
+          <hr className="border-pink"/>
+          <Accordion
+            title="Why Solana?"
             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
           />
         </div>
