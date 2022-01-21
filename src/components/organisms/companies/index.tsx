2433156f4623b39458830babb9c3e6846697aab7
ABDELAZIZ BOUNHAR <40790121+BounharAbdelaziz@users.noreply.github.com>
2022-01-21 16:31:23 +0100

    companies section

---
+++ src/components/organisms/companies/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ListTeam from '../../molecules/list/ListTeam'
+import TitleSection from '../../molecules/TitleSection'
+
+const Companies : React.FC = () => {
+    return (
+        <div className='container mx-auto'>
+            <TitleSection
+                title='Our Team worked in these companies'
+                subtitle=''
+                classes='text-center mb-10'
+            />
+
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Companies
