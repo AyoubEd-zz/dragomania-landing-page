cb903c6c7883fbb88e5f9aaa4fe9bb8a20f7a6c9
ABDELAZIZ BOUNHAR <40790121+BounharAbdelaziz@users.noreply.github.com>
2022-01-21 16:30:30 +0100

    change carousel

--- src/components/molecules/list/ListAuction.tsx
+++ src/components/molecules/list/ListAuction.tsx
@@ -4,6 +4,8 @@ import fire from '../../utils/images/nft/fire.gif'
 import electricity from '../../utils/images/nft/electricity.gif'
 import plant from '../../utils/images/nft/plant.gif'
 import sound from '../../utils/images/nft/sound.gif'
+import steel from '../../utils/images/nft/steel.gif'
+import water from '../../utils/images/nft/water.gif'
 import fireIcon from '../../utils/images/elements/fire.png'
 import waterIcon from '../../utils/images/elements/water.png'
 import steelIcon from '../../utils/images/elements/steel.png'
@@ -13,7 +15,7 @@ import electricIcon from '../../utils/images/elements/electric.png'
 
 const ListAuction : React.FC = () => {
     return (
-        <div className='flex flex-row flex-wrap'>
+        <div className='flex flex-row flex-initial'>
                 <div className='flex-grow-0 flex-shrink w-full md:w-6/12 xl:w-3/12 p-3'>
                     <CardAuction
                         author="gachalifes"
@@ -60,6 +62,30 @@ const ListAuction : React.FC = () => {
                         date="15 october 2021"
                         expired={1639152000}
                         icon={natureIcon}
+                    />
+                </div>
+                <div className='flex-grow-0 flex-shrink w-full md:w-6/12 xl:w-3/12 p-3'>
+                    <CardAuction
+                        author="gachalifes"
+                        authorVerified={true}
+                        title="Water"
+                        bidPrice={0.00234}
+                        image={water}
+                        date="15 october 2021"
+                        expired={1639152000}
+                        icon={waterIcon}
+                    />
+                </div>
+                <div className='flex-grow-0 flex-shrink w-full md:w-6/12 xl:w-3/12 p-3'>
+                    <CardAuction
+                        author="gachalifes"
+                        authorVerified={true}
+                        title="Metal"
+                        bidPrice={0.00234}
+                        image={steel}
+                        date="15 october 2021"
+                        expired={1639152000}
+                        icon={steelIcon}
                     />
                 </div>
         </div>
