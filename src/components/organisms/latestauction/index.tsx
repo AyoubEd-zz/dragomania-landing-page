2bfcf5df1fd5331c9a038cf7ddf6fd59dd2fef52
ABDELAZIZ BOUNHAR <40790121+BounharAbdelaziz@users.noreply.github.com>
2022-01-21 14:13:50 +0100

    update dragos title

--- src/components/organisms/latestauction/index.tsx
+++ src/components/organisms/latestauction/index.tsx
@@ -1,30 +1,22 @@
 import React from 'react'
 import ListAuction from '../../molecules/list/ListAuction'
-import ListAuctionTwo from '../../molecules/list/ListAuction2'
 import TitleSection from '../../molecules/TitleSection'
 
 import "react-responsive-carousel/lib/styles/carousel.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
 import "../../utils/styles/carousel.css"
 
 const LatestAuction : React.FC = () => {
     return (
         <div className='container mx-auto'>
             <TitleSection
-                title='The Dragos'
+                title='Genesis Dragos'
                 subtitle='Meet the antagonists'
                 classes='text-center mb-10'
             />
 
-            <Carousel showStatus={false} showArrows={true} className='mx-8'>
-                <ListAuction />
-                <ListAuctionTwo />
-            </Carousel>
+            <ListAuction />
 
             <blockquote className="relative p-4 text-xl italic text-white quote">
-                {/* <div className="stylistic-quote-mark" aria-hidden="true">
-                    &ldquo;
-                </div> */}
                 <cite className="flex items-center">
                     <p className="mb-4 text-center">
                         Dragos are little fierce dragons living on the Solana Blockchain. There are different classes of Dragos that players can collect with varying body parts and appearances. Some rarer than others. Body parts will unlock abilities that vary across games.
